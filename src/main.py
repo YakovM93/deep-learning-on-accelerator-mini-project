b4d606574d1e43652f1648a53323a8a3a67e1e31
Oz Diamond <ozdiamond@outlook.co.il>
2025-03-20 12:54:14 +0200

    Yakov's New Model
    Updated Model that gives good accuracy scores

--- src/main.py
+++ src/main.py
@@ -1,56 +1,316 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, random_split
 from torchvision import datasets, transforms
-import numpy as np
-from matplotlib import pyplot as plt
-from utils import plot_tsne
+
 import numpy as np
 import random
 import argparse
+import matplotlib.pyplot as plt
 
-NUM_CLASSES = 10
+# Import your models from model.py
+from models.model import (ConvAutoencoderMNIST, ConvAutoencoderCIFAR, LatentClassifier,
+                          ConvEncoderClassifierMNIST, ConvEncoderClassifierCIFAR)
+# Import utility functions from utils.py
+from utils import plot_tsne
 
 def freeze_seeds(seed=0):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-def get_args():   
+    torch.cuda.manual_seed_all(seed)
+
+def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--seed', default=0, type=int, help='Seed for random number generators')
-    parser.add_argument('--data-path', default="/datasets/cv_datasets/data", type=str, help='Path to dataset')
-    parser.add_argument('--batch-size', default=8, type=int, help='Size of each batch')
-    parser.add_argument('--latent-dim', default=128, type=int, help='encoding dimension')
-    parser.add_argument('--device', default='cuda' if torch.cuda.is_available() else 'cpu', type=str, help='Default device to use')
-    parser.add_argument('--mnist', action='store_true', default=False,
-                        help='Whether to use MNIST (True) or CIFAR10 (False) data')
-    parser.add_argument('--self-supervised', action='store_true', default=False,
-                        help='Whether train self-supervised with reconstruction objective, or jointly with classifier for classification objective.')
+    parser.add_argument('--seed', default=0, type=int, help='Seed for reproducibility')
+    parser.add_argument('--data-path', default="./data", type=str, help='Path to dataset')
+    parser.add_argument('--batch-size', default=32, type=int, help='Batch size')
+    parser.add_argument('--latent-dim', default=128, type=int, help='Latent dimension')
+    parser.add_argument('--lr', default=1e-3, type=float, help='Learning rate')
+    parser.add_argument('--epochs-ae', default=35, type=int, help='Number of epochs for autoencoder pretraining')
+    parser.add_argument('--epochs-clf', default=170, type=int, help='Number of epochs for classifier training (self-supervised mode)')
+    parser.add_argument('--epochs-cg', default=20, type=int, help='Number of epochs for classification guided training')
+    parser.add_argument('--mnist', action='store_true', default=False, help='Use MNIST if True, else CIFAR10')
+    parser.add_argument('--device', default='cuda' if torch.cuda.is_available() else 'cpu', type=str)
+    parser.add_argument('--mode', default='self_supervised', choices=['self_supervised', 'classification_guided'],
+                        help='Training mode: self_supervised (1.2.1) or classification_guided (1.2.2)')
+    # Optionally add a flag to switch optimizers or schedulers
+    parser.add_argument('--optimizer', default='adamw', type=str, help='Optimizer: adam / adamw / sgd / rmsprop')
     return parser.parse_args()
-    
+
+def reconstruction_loss(x_recon, x):
+    return nn.functional.l1_loss(x_recon, x)
+
+def train_autoencoder(model, dataloader, optimizer, device, scheduler=None):
+    model.train()
+    total_loss = 0.0
+    for images, _ in dataloader:
+        images = images.to(device)
+        optimizer.zero_grad()
+        recon = model(images)
+        loss = reconstruction_loss(recon, images)
+        loss.backward()
+        optimizer.step()
+    if scheduler is not None:
+        scheduler.step()
+    return loss.item()
+
+def evaluate_autoencoder(model, dataloader, device):
+    model.eval()
+    total_loss = 0.0
+    with torch.no_grad():
+        for images, _ in dataloader:
+            images = images.to(device)
+            recon = model(images)
+            loss = reconstruction_loss(recon, images)
+            total_loss += loss.item() * images.size(0)
+    return total_loss / len(dataloader.dataset)
+
+def train_classifier(frozen_encoder, classifier, dataloader, optimizer, device, scheduler=None):
+    frozen_encoder.eval()
+    classifier.train()
+    criterion = nn.CrossEntropyLoss()
+    total_loss = 0.0
+    for images, labels in dataloader:
+        images = images.to(device)
+        labels = labels.to(device)
+        optimizer.zero_grad()
+        with torch.no_grad():
+            z = frozen_encoder.encode(images)
+        logits = classifier(z)
+        loss = criterion(logits, labels)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item() * images.size(0)
+    if scheduler is not None:
+        scheduler.step()
+    return total_loss / len(dataloader.dataset)
+
+def evaluate_classifier(frozen_encoder, classifier, dataloader, device):
+    frozen_encoder.eval()
+    classifier.eval()
+    criterion = nn.CrossEntropyLoss()
+    total_loss = 0.0
+    total_correct = 0
+    with torch.no_grad():
+        for images, labels in dataloader:
+            images = images.to(device)
+            labels = labels.to(device)
+            z = frozen_encoder.encode(images)
+            logits = classifier(z)
+            loss = criterion(logits, labels)
+            total_loss += loss.item() * images.size(0)
+            total_correct += (logits.argmax(dim=1) == labels).sum().item()
+    avg_loss = total_loss / len(dataloader.dataset)
+    avg_acc = total_correct / len(dataloader.dataset)
+    return avg_loss, avg_acc
+
+def train_classification_guided(model, dataloader, optimizer, device, scheduler=None):
+    model.train()
+    criterion = nn.CrossEntropyLoss()
+    total_loss = 0.0
+    for images, labels in dataloader:
+        images = images.to(device)
+        labels = labels.to(device)
+        optimizer.zero_grad()
+        logits = model(images)
+        loss = criterion(logits, labels)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item() * images.size(0)
+    if scheduler is not None:
+        scheduler.step()
+    return total_loss / len(dataloader.dataset)
+
+def evaluate_classification_guided(model, dataloader, device):
+    model.eval()
+    criterion = nn.CrossEntropyLoss()
+    total_loss = 0.0
+    total_correct = 0
+    with torch.no_grad():
+        for images, labels in dataloader:
+            images = images.to(device)
+            labels = labels.to(device)
+            logits = model(images)
+            loss = criterion(logits, labels)
+            total_loss += loss.item() * images.size(0)
+            total_correct += (logits.argmax(dim=1) == labels).sum().item()
+    avg_loss = total_loss / len(dataloader.dataset)
+    avg_acc = total_correct / len(dataloader.dataset)
+    return avg_loss, avg_acc
+
+def visualize_reconstructions(model, dataloader, device, n_images=5):
+    model.eval()
+    images, _ = next(iter(dataloader))
+    images = images[:n_images].to(device)
+    with torch.no_grad():
+        recon = model(images)
+    images = images.cpu().numpy()
+    recon = recon.cpu().numpy()
+    fig, axes = plt.subplots(2, n_images, figsize=(3*n_images, 6))
+    for i in range(n_images):
+        if images.shape[1] == 1:  # grayscale
+            axes[0, i].imshow(images[i].squeeze(), cmap="gray")
+            axes[1, i].imshow(recon[i].squeeze(), cmap="gray")
+        else:
+            axes[0, i].imshow(np.transpose(images[i], (1,2,0)))
+            axes[1, i].imshow(np.transpose(recon[i], (1,2,0)))
+        axes[0, i].set_title("Original")
+        axes[1, i].set_title("Reconstruction")
+        axes[0, i].axis("off")
+        axes[1, i].axis("off")
+    plt.tight_layout()
+    plt.show()
+
+def linear_interpolation(model, dataloader, device, steps=10):
+    model.eval()
+    images, _ = next(iter(dataloader))
+    if images.size(0) < 2:
+        print("Not enough images for interpolation.")
+        return
+    img1 = images[0].unsqueeze(0).to(device)
+    img2 = images[1].unsqueeze(0).to(device)
+    with torch.no_grad():
+        z1 = model.encode(img1)
+        z2 = model.encode(img2)
+    alphas = np.linspace(0, 1, steps)
+    interpolated = []
+    for alpha in alphas:
+        z_interp = (1 - alpha) * z1 + alpha * z2
+        with torch.no_grad():
+            x_interp = model.decode(z_interp)
+        interpolated.append(x_interp.squeeze(0).cpu().numpy())
+    fig, axes = plt.subplots(1, steps, figsize=(3*steps, 3))
+    for i, img in enumerate(interpolated):
+        if img.shape[0] == 1:
+            axes[i].imshow(img.squeeze(), cmap="gray")
+        else:
+            axes[i].imshow(np.transpose(img, (1,2,0)))
+        axes[i].set_title(f"alpha={alphas[i]:.2f}")
+        axes[i].axis("off")
+    plt.tight_layout()
+    plt.show()
+
+def main():
+    args = get_args()
+    device = args.device
+    freeze_seeds(args.seed)
+
+    # Data Augmentation for training
+    if args.mnist:
+        train_transform = transforms.Compose([
+            transforms.RandomRotation(degrees=10),
+            transforms.ToTensor()
+        ])
+        test_transform = transforms.Compose([
+            transforms.ToTensor()
+        ])
+        full_train_dataset = datasets.MNIST(root=args.data_path, train=True, download=True, transform=train_transform)
+        test_dataset = datasets.MNIST(root=args.data_path, train=False, download=True, transform=test_transform)
+    else:
+        train_transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor()
+        ])
+        test_transform = transforms.Compose([
+            transforms.ToTensor()
+        ])
+        full_train_dataset = datasets.CIFAR10(root=args.data_path, train=True, download=True, transform=train_transform)
+        test_dataset = datasets.CIFAR10(root=args.data_path, train=False, download=True, transform=test_transform)
+
+    val_size = 5000
+    train_size = len(full_train_dataset) - val_size
+    train_dataset, val_dataset = random_split(full_train_dataset, [train_size, val_size])
+    # For val dataset, we can use test_transform to avoid augmentation in validation
+    val_dataset.dataset.transform = test_transform
+
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    val_loader   = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False)
+    test_loader  = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
+
+    # Helper function to create optim & scheduler
+    def create_optimizer_scheduler(model_params, lr, opt_type='adamw', T_max=10):
+        if opt_type.lower() == 'adam':
+            optimizer = optim.Adam(model_params, lr=lr)
+        elif opt_type.lower() == 'adamw':
+            optimizer = optim.AdamW(model_params, lr=lr)
+        elif opt_type.lower() == 'rmsprop':
+            optimizer = optim.RMSprop(model_params, lr=lr)
+        elif opt_type.lower() == 'sgd':
+            optimizer = optim.SGD(model_params, lr=lr, momentum=0.85)
+        else:
+            optimizer = optim.AdamW(model_params, lr=lr)
+
+        # Example: CosineAnnealingLR over the number of epochs
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=T_max)
+        return optimizer, scheduler
+
+    # Mode Selection
+    if args.mode == 'self_supervised':
+        # Self-Supervised Autoencoder Training (1.2.1)
+        if args.mnist:
+            autoencoder = ConvAutoencoderMNIST(latent_dim=args.latent_dim).to(device)
+        else:
+            autoencoder = ConvAutoencoderCIFAR(latent_dim=args.latent_dim).to(device)
+
+        optimizer_ae, scheduler_ae = create_optimizer_scheduler(autoencoder.parameters(), args.lr, args.optimizer, T_max=args.epochs_ae)
+        print("Training Autoencoder (Self-Supervised)...")
+        for epoch in range(args.epochs_ae):
+            train_loss = train_autoencoder(autoencoder, train_loader, optimizer_ae, device, scheduler_ae)
+            val_loss = evaluate_autoencoder(autoencoder, val_loader, device)
+            print(f"[AE Epoch {epoch+1}/{args.epochs_ae}] Train MAE={train_loss:.4f} | Val MAE={val_loss:.4f}")
+
+        test_mae = evaluate_autoencoder(autoencoder, test_loader, device)
+        print(f"Test Reconstruction MAE: {test_mae:.4f}")
+
+        # Train Classifier on Frozen Encoder
+        for param in autoencoder.parameters():
+            param.requires_grad = False
+        classifier = LatentClassifier(latent_dim=args.latent_dim, num_classes=10).to(device)
+
+        optimizer_clf, scheduler_clf = create_optimizer_scheduler(classifier.parameters(), args.lr, args.optimizer, T_max=args.epochs_clf)
+        print("Training Classifier on Frozen Encoder...")
+        for epoch in range(args.epochs_clf):
+            train_clf_loss = train_classifier(autoencoder, classifier, train_loader, optimizer_clf, device, scheduler_clf)
+            val_clf_loss, val_clf_acc = evaluate_classifier(autoencoder, classifier, val_loader, device)
+            print(f"[CLF Epoch {epoch+1}/{args.epochs_clf}] Train Loss={train_clf_loss:.4f} | Val Loss={val_clf_loss:.4f}, Acc={val_clf_acc*100:.2f}%")
+
+        test_clf_loss, test_clf_acc = evaluate_classifier(autoencoder, classifier, test_loader, device)
+        print(f"Test Classifier Loss={test_clf_loss:.4f}, Test Accuracy={test_clf_acc*100:.2f}%")
+
+        # Qualitative Evaluations
+        print("Visualizing Reconstructions...")
+        visualize_reconstructions(autoencoder, test_loader, device, n_images=5)
+        print("Performing Linear Interpolation on two images...")
+        linear_interpolation(autoencoder, test_loader, device, steps=10)
+
+        print("Generating t-SNE plots for Self-Supervised Model...")
+        plot_tsne(autoencoder.encode, test_loader, device, image_tsne_path='tsne_img_selfsup.png', latent_tsne_path='tsne_latent_selfsup.png')
+
+    elif args.mode == 'classification_guided':
+        # Classification-Guided Training (1.2.2)
+        if args.mnist:
+            model_cg = ConvEncoderClassifierMNIST(latent_dim=args.latent_dim, num_classes=10).to(device)
+        else:
+            model_cg = ConvEncoderClassifierCIFAR(latent_dim=args.latent_dim, num_classes=10).to(device)
+
+        optimizer_cg, scheduler_cg = create_optimizer_scheduler(model_cg.parameters(), args.lr, args.optimizer, T_max=args.epochs_cg)
+        print("Training Classification-Guided Model...")
+        for epoch in range(args.epochs_cg):
+            train_loss = train_classification_guided(model_cg, train_loader, optimizer_cg, device, scheduler_cg)
+            val_loss, val_acc = evaluate_classification_guided(model_cg, val_loader, device)
+            print(f"[Epoch {epoch+1}/{args.epochs_cg}] Train Loss={train_loss:.4f} | Val Loss={val_loss:.4f}, Val Acc={val_acc*100:.2f}%")
+
+        test_loss, test_acc = evaluate_classification_guided(model_cg, test_loader, device)
+        print(f"Test Loss={test_loss:.4f}, Test Accuracy={test_acc*100:.2f}%")
+
+        # t-SNE for Classification-Guided Model using its encoder
+        def encode_fn(x):
+            return model_cg.encode(x)
+        print("Generating t-SNE plots for Classification-Guided Model...")
+        plot_tsne(encode_fn, test_loader, device, image_tsne_path='tsne_img_cg.png', latent_tsne_path='tsne_latent_cg.png')
 
 if __name__ == "__main__":
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  #one possible convenient normalization. You don't have to use it.
-    ])
-
-    args = get_args()
-    freeze_seeds(args.seed)
-                
-                                           
-    if args.mnist:
-        train_dataset = datasets.MNIST(root=args.data_path, train=True, download=False, transform=transform)
-        test_dataset = datasets.MNIST(root=args.data_path, train=False, download=False, transform=transform)
-    else:
-        train_dataset = datasets.CIFAR10(root=args.data_path, train=True, download=True, transform=transform)
-        test_dataset = datasets.CIFAR10(root=args.data_path, train=False, download=True, transform=transform)
-        
-    # When you create your dataloader you should split train_dataset or test_dataset to leave some aside for validation
-
-    #this is just for the example. Simple flattening of the image is probably not the best idea                                        
-    encoder_model = torch.nn.Linear(32*32*3,args.latent_dim).to(args.device)
-    decoder_model = torch.nn.Linear(args.latent_dim,32*32*3 if args.self_supervised else NUM_CLASSES).to(args.device) 
-
-    sample = train_dataset[0][0][None].to(args.device) #This is just for the example - you should use a dataloader
-    output = decoder_model(encoder_model(sample.flatten()))
-    print(output.shape)
-
+    main()
--- src/utils.py
+++ src/utils.py
@@ -1,54 +1,56 @@
 import torch
 import numpy as np
 from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
 
-def plot_tsne(model, dataloader, device):
-    '''
-    model - torch.nn.Module subclass. This is your encoder model
-    dataloader - test dataloader to over over data for which you wish to compute projections
-    device - cuda or cpu (as a string)
-    '''
-    model.eval()
-    
+def plot_tsne(encode_fn, dataloader, device,
+              image_tsne_path='image_tsne.png',
+              latent_tsne_path='latent_tsne.png'):
+    """
+    encode_fn - A function that takes in a batch of images and returns
+                their latent embeddings (for example: autoencoder.encode).
+    dataloader - Dataloader over which to compute and plot t-SNE.
+    device - 'cpu' or 'cuda'
+    image_tsne_path - File path for saving the t-SNE in image-space.
+    latent_tsne_path - File path for saving the t-SNE in latent-space.
+    """
     images_list = []
     labels_list = []
     latent_list = []
-    
+
     with torch.no_grad():
-        for data in dataloader:
-            images, labels = data
+        for images, labels in dataloader:
             images, labels = images.to(device), labels.to(device)
-            
-            #approximate the latent space from data
-            latent_vector = model(images)
-            
+            z = encode_fn(images)
             images_list.append(images.cpu().numpy())
             labels_list.append(labels.cpu().numpy())
-            latent_list.append(latent_vector.cpu().numpy())
-    
+            latent_list.append(z.cpu().numpy())
+
     images = np.concatenate(images_list, axis=0)
     labels = np.concatenate(labels_list, axis=0)
     latent_vectors = np.concatenate(latent_list, axis=0)
-    
-    # Plot TSNE for latent space
+
+    # t-SNE on latent vectors
     tsne_latent = TSNE(n_components=2, random_state=0)
     latent_tsne = tsne_latent.fit_transform(latent_vectors)
-    
+
     plt.figure(figsize=(8, 6))
-    scatter = plt.scatter(latent_tsne[:, 0], latent_tsne[:, 1], c=labels, cmap='tab10', s=10)  # Smaller points
+    scatter = plt.scatter(latent_tsne[:, 0], latent_tsne[:, 1], c=labels, cmap='tab10', s=10)
     plt.colorbar(scatter)
     plt.title('t-SNE of Latent Space')
-    plt.savefig('latent_tsne.png')
+    plt.grid(True)
+    plt.savefig(latent_tsne_path)
     plt.close()
-    
-    #plot image domain tsne
-    tsne_image = TSNE(n_components=2, random_state=42)
+
+    # t-SNE on images (flattened)
     images_flattened = images.reshape(images.shape[0], -1)
+    tsne_image = TSNE(n_components=2, random_state=42)
     image_tsne = tsne_image.fit_transform(images_flattened)
-    
+
     plt.figure(figsize=(8, 6))
-    scatter = plt.scatter(image_tsne[:, 0], image_tsne[:, 1], c=labels, cmap='tab10', s=10)  
+    scatter = plt.scatter(image_tsne[:, 0], image_tsne[:, 1], c=labels, cmap='tab10', s=10)
     plt.colorbar(scatter)
     plt.title('t-SNE of Image Space')
-    plt.savefig('image_tsne.png')
+    plt.grid(True)
+    plt.savefig(image_tsne_path)
     plt.close()
